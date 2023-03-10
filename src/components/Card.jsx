da7cb783b5910ea697c13e0a1070b914b5400de6
Renchester Ramos <renchesterjramos@gmail.com>
2023-03-11 02:19:53 +0800

    Add Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+function Card(props) {
+  const { id, img, name, handleClick, handleTransitionEnd } = props;
+
+  return (
+    <button type="button" className="card" onClick={(e) => handleClick(e, id)}>
+      <div className="card__image-container">
+        <img src={img} alt={name} className="card__image" />
+      </div>
+      <span>{name}</span>
+    </button>
+  );
+}
+
+export default Card;
