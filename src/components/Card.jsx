8be28a55fa58d071d43f5fbd7eaa3b803f5b1e8b
Renchester Ramos <renchesterjramos@gmail.com>
2023-03-13 07:52:58 +0800

    Add card rotation animation and assets

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,37 @@
+import { useEffect } from 'react';
+
 function Card(props) {
-  const { id, img, name, handleClick } = props;
+  const { char, handleClick, isCardFlipped, setCardFlip } = props;
+  const { id, img, name } = char;
+
+  const CARD_TIMEOUT = 1000;
+  const ANIMATION_DURATION = CARD_TIMEOUT / 2;
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCardFlip(true);
+    }, CARD_TIMEOUT);
+  }, [isCardFlipped]);
 
   return (
-    <button type="button" className="card" onClick={(e) => handleClick(e, id)}>
-      <div className="card__image-container">
-        <img src={img} alt={name} className="card__image" />
-      </div>
-      <span>{name}</span>
-    </button>
+    <div className={isCardFlipped ? 'card card--flipped' : 'card'}>
+      <button
+        type="button"
+        className="card-side front"
+        onClick={(e) => {
+          setCardFlip(() => false);
+          setTimeout(() => {
+            handleClick(e, id);
+          }, ANIMATION_DURATION);
+        }}
+      >
+        <div className="card__image-container">
+          <img src={img} alt={name} className="card__image" />
+        </div>
+        <span className="card__image-title">{name}</span>
+      </button>
+      <div className="card-side back" />
+    </div>
   );
 }
 
